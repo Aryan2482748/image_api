edcc65db4ae03b11c4873eb130c7daded89ff232
Aryan2482748 <87892275+Aryan2482748@users.noreply.github.com>
2025-02-02 10:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Response
+from fastapi.responses import StreamingResponse
 from typing import List
 import io
 import cv2
 import numpy as np
 from PIL import Image
+
 app = FastAPI()
-@app.post("/process_image/")
-async def process_image(image: UploadFile = File(...)):
+
+@app.post("/process_image/stream")
+async def process_image_stream(image: UploadFile = File(...)):
     # Read the image file
     contents = await image.read()
     nparr = np.fromstring(contents, np.uint8)
@@ -21,6 +24,8 @@ async def process_image(image: UploadFile = File(...)):
     # Detect edges
     edges = cv2.Canny(blur_image, 10, 70)
 
-    # Convert the processed image to bytes for response
+    # Convert the processed image to bytes for streaming
     res, im_png = cv2.imencode(".png", edges)
-    return {"image_bytes": im_png.tobytes()}
+    image_bytes = io.BytesIO(im_png.tobytes())
+
+    return StreamingResponse(image_bytes, media_type="image/png")
